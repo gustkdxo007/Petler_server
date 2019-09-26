09084e77a7da9f7950d8df107ebf6a4f05dbb01c
leeeunji <ej.lee005@gmail.com>
2019-09-26 22:12:09 +0900

    :sparkles: Add login with jwt

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,5 +1,5 @@
-/* eslint-disable no-unused-expressions */
 import dotenv from "dotenv";
+import jwt from "jsonwebtoken";
 import models from "../models";
 import hash from "../auth/hash";
 
@@ -24,15 +24,28 @@ const resolvers = {
       });
       return result[0];
     },
+    login: async (_, { email, password }) => {
+      const user = await models.user.findOne({
+        where: { email },
+      });
+      if (!user) {
+        throw new Error("가입되지 않은 아이디입니다");
+      }
+      const valid = hash(password);
+      if (valid !== user.dataValues.password) {
+        throw new Error("비밀번호가 일치하지 않습니다");
+      }
+      const token = jwt.sign({ email, valid }, process.env.JWT_SECRET, { expiresIn: "1h" });
+      return { token, user };
+    },
   },
   Mutation: {
     signUp: async (_, args) => {
-      const a = await models.user.findOne({
+      const signed = await models.user.findOne({
         where: { email: args.signupInput.email },
       });
-      if (a) {
-        console.log("이미 가입된 사용자입니다.");
-        return null;
+      if (signed) {
+        throw new Error("이미 가입된 사용자입니다.");
       }
       const newUser = models.user.create({
         name: args.signupInput.name,
